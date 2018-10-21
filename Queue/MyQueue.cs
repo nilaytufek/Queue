1d7ae139276bb4f9068b9b01d5e7524592f7c36f
nilay tufek <ntufek@gmail.com>
2018-10-21 19:41:12 +0100

    first commit

---
+++ Queue/MyQueue.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+
+/* @author: nilay tufek
+ * ntufek@gmail.com
+ */
+
+namespace Queue
+{
+    
+    public class Node<V>
+    {
+        public V value;            
+        public Node<V> NextNode = null;
+
+        public Node(V v)
+        {
+            value = v;
+        }
+
+        public static implicit operator Node<V>(int v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    
+    class MyQueue<V>
+    {
+        public Node<V> first = null;
+        public Node<V> last = null;
+
+        public MyQueue()
+        {
+            first = null;
+            last = first;
+        }
+        public void Enqueue(Node<V> node)
+        {
+            if (first == null)
+            {
+                first = node;
+            }
+            else
+            {
+                last.NextNode = node;
+            }
+            last = node;
+        }
+
+        public V Dequeue()
+        {
+            V deQ = first.value;
+
+            first = first.NextNode;
+            return deQ;
+        }
+
+        public V[] GetQ()
+        {
+            V[] arr = new V[0];
+            Node<V> tmp = first;
+            int i = 0;
+            while (tmp != null)
+            {
+                Array.Resize(ref arr, i + 1);
+                arr[i] = tmp.value;
+                tmp = tmp.NextNode;
+                i++;
+
+            }
+            return arr;
+
+
+        }
+
+    }
+}
---
+++ Queue/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyQueue<int> myQueue = new MyQueue<int>();
+            Node<int> e1 = new Node<int>(1);
+            myQueue.Enqueue(e1);
+            Node<int> e2 = new Node<int>(2);
+            myQueue.Enqueue(e2);
+            Node<int> e3 = new Node<int>(3);
+            myQueue.Enqueue(e3);
+            //print queue
+            Console.WriteLine("myQueue: " + string.Join(" ", myQueue.GetQ()));
+            //dequeue
+            int retVal = myQueue.Dequeue();
+            Console.WriteLine("Dequeued value: " + retVal);
+            //print queue again
+            Console.WriteLine("myQueue: " + string.Join(" ", myQueue.GetQ()));
+            Console.ReadLine();
+
+        }
+    }
+}
